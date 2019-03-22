a834d4e3a446488df98bca01c07ad52dc7e74e69
Leland Jansen <hello@lelandjansen.com>
2019-03-22 11:13:36 -0600

    grid point cloud

--- src/visualize.py
+++ src/visualize.py
@@ -3,6 +3,7 @@ import numpy as np
 import pptk
 import time
 from math import pi
+from math import sqrt
 import os
 import sys
 from PIL import Image
@@ -10,8 +11,6 @@ import datetime
 
 sleep = 0.2
 dir_path = os.path.dirname(os.path.realpath(__file__))
-
-# xyz = pptk.rand(100, 3)
 
 path = os.path.join(dir_path,
         '../data/sydney-urban-objects-dataset/objects/car.23.17038.csv')
@@ -22,37 +21,44 @@ x_center = (x_min + x_max) / 2.0
 y_center = (y_min + y_max) / 2.0
 z_center = (z_min + z_max) / 2.0
 v = pptk.viewer(xyz, xyz[:, 0])
-
+ 
 images = []
-for phi in [-pi/12, 0, pi/12]:
-    for theta in [-pi/12, 0, pi/12]:
-        for r in [5, 5.5, 6]:
-            v.set(lookat = [x_center, y_center, z_center],
-                  phi = phi,
-                  theta = theta,
-                  r = r,
-                  point_size = 0.01,
-                  bg_color = [0, 0, 0, 0],
-                  show_grid = False,
-                  show_info = False,
-                  show_axis = False)
-            time.sleep(sleep)
-            path = os.path.join(dir_path, '../out/tmp',
-                '{}-{}-{}.png'.format(phi, theta, r))
-            images.append(path)
-            v.capture(path)
+for phi in np.arange(0, 2 * pi, pi / 4.0):
+    for theta in np.arange(0, 2 * pi, pi / 4.0):
+        v.set(lookat = [x_center, y_center, z_center],
+              phi = phi,
+              theta = theta,
+              r = 6.5,
+              point_size = 0.02,
+              bg_color = [0, 0, 0, 0],
+              show_grid = False,
+              show_info = False,
+              show_axis = False)
+        time.sleep(sleep)
+        path = os.path.join(dir_path, '../out/tmp',
+            '{}-{}.png'.format(phi, theta))
+        images.append(path)
+        v.capture(path)
 time.sleep(sleep)
 v.close()
 
 images = map(Image.open, images) 
 widths, heights = zip(*(image.size for image in images))
 total_width = sum(widths)
-max_height = max(heights)
-new_image = Image.new('RGB', (total_width, max_height))
-x_offset = 0
-for image in images:
-    new_image.paste(image, (x_offset, 0))
-    x_offset += image.size[0]
+total_height = sum(heights)
+n = int(sqrt(len(images)))
+images = [images[i:i+n] for i in xrange(0, len(images), n)]
+offset = images[0][0].size[0]
+new_image = Image.new('RGB', (offset * len(images), offset * len(images[0])))
+y_offset = 0
+for row in images:
+    x_offset = 0
+    for image in row:
+        new_image.paste(image, (x_offset, y_offset))
+        x_offset += offset
+    y_offset += offset
 path = os.path.join(dir_path, '../out/',
         '{}.png'.format(datetime.datetime.now().strftime("%Y%m%d%H%M%S")))
+new_image.thumbnail((new_image.size[0] / 3, new_image.size[1] / 3),
+                    Image.ANTIALIAS)
 new_image.save(path)
