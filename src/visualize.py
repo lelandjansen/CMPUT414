ff6252e6f2aed5cfc624875d39098019c6070003
Leland Jansen <hello@lelandjansen.com>
2019-03-21 21:00:13 -0600

    Parse csv (bad code)

--- src/visualize.py
+++ src/visualize.py
@@ -8,10 +8,35 @@ import sys
 from PIL import Image
 import datetime
 
+sleep = 0.2
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 xyz = pptk.rand(100, 3)
 v = pptk.viewer(xyz, xyz[:, 2])
+
+# path = os.path.join(dir_path,
+#         '../data/sydney-urban-objects-dataset/objects/car.61.25322.csv')
+# xyz = np.loadtxt(open(path, 'rb'), delimiter=',', usecols=range(1,4))
+# x_min, y_min, z_min = xyz.min(axis=0)
+# x_max, y_max, z_max = xyz.max(axis=0)
+# x_center = (x_min + x_max) / 2.0
+# y_center = (y_min + y_max) / 2.0
+# z_center = (z_min + z_max) / 2.0
+# v.set(lookat = [x_center, y_center, z_center],
+# #      phi = 0,
+# #      theta = pi,
+#       r = 200,
+#       view = [x_center, y_center, z_center],
+# #      up = [x_center, y_center, z_center],
+#       point_size = 1,
+#       bg_color = [0, 0, 0, 0],
+#       show_grid = False,
+#       show_info = True,
+#       show_axis = False)
+# 
+# while True:
+#     pass
+
 images = []
 for phi in [-pi/12, 0, pi/12]:
     for theta in [-pi/12, 0, pi/12]:
@@ -25,12 +50,12 @@ for phi in [-pi/12, 0, pi/12]:
                   show_grid = False,
                   show_info = False,
                   show_axis = False)
-            time.sleep(0.1)
+            time.sleep(sleep)
             path = os.path.join(dir_path, '../out/tmp',
                 '{}-{}-{}.png'.format(phi, theta, r))
             images.append(path)
             v.capture(path)
-time.sleep(0.1)
+time.sleep(sleep)
 v.close()
 
 images = map(Image.open, images) 
