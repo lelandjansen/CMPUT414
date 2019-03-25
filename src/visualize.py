f055a5a308a49f8180e22afddab08c4a5506df2f
Leland Jansen <hello@lelandjansen.com>
2019-03-24 22:27:01 -0600

    Generate one label file as test

--- src/visualize.py
+++ src/visualize.py
@@ -10,66 +10,110 @@ from PIL import Image
 import datetime
 import math
 
-sleep = 0.2
-dir_path = os.path.dirname(os.path.realpath(__file__))
+SLEEP = 0.2
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-path = os.path.join(dir_path,
-        '../data/sydney-urban-objects-dataset/objects/car.23.17038.csv')
-xyz = np.loadtxt(open(path, 'rb'), delimiter=',', usecols=range(3,6))
-x_min, y_min, z_min = xyz.min(axis=0)
-x_max, y_max, z_max = xyz.max(axis=0)
-x_center = (x_min + x_max) / 2.0
-y_center = (y_min + y_max) / 2.0
-z_center = (z_min + z_max) / 2.0
-xyz = np.array([(p[0] - x_center, p[1] - y_center, p[2] - z_center) for p in xyz])
-v = pptk.viewer(xyz, xyz[:, 0])
- 
-images = []
-for phi in np.arange(0.0, pi, pi / 8.0):
-    for theta in np.arange(0.0, 2 * pi, pi / 8.0):
-        r = 6.5
-        camera_x = r * math.sin(phi) * math.cos(theta)
-        camera_y = r * math.sin(phi) * math.sin(theta)
-        camera_z = r * math.cos(phi)
-        print camera_x, camera_y, camera_z
-        colors = []
-        for p in xyz:
-            distance = sqrt((camera_x-p[0])**2 + (camera_y-p[1])**2 + (camera_z-p[2])**2)
-            colors.append(distance)
-        v.attributes(colors)
-        v.color_map('cool')
-        v.set(phi = phi,
-              theta = theta,
-              r = r,
-              point_size = 0.4,
-              bg_color = [0, 0, 0, 0],
-              show_grid = False,
-              show_info = False,
-              show_axis = False)
-        time.sleep(sleep)
-        path = os.path.join(dir_path, '../out/tmp',
-            '{}-{}.png'.format(phi, theta))
-        images.append(path)
-        v.capture(path)
-time.sleep(sleep)
-v.close()
+def enumerate_files():
+    path = os.path.join(DIR_PATH, '../data/sydney-urban-objects-dataset/objects/')
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    files  = [f for f in files if f.endswith('.csv')]
+    vehicles = [f for f in files if '4wd' in f or 'car' in f or 'truck' in f or 'van' in f]
+    pedestrians = [f for f in files if 'pedestrian' in f]
+    return (vehicles, pedestrians)
 
-images = map(Image.open, images) 
-widths, heights = zip(*(image.size for image in images))
-total_width = sum(widths)
-total_height = sum(heights)
-n = int(sqrt(len(images)))
-images = [images[i:i+n] for i in xrange(0, len(images), n)]
-offset = images[0][0].size[0]
-new_image = Image.new('RGB', (offset * len(images), offset * len(images[0])))
-y_offset = 0
-for row in images:
-    x_offset = 0
-    for image in row:
-        new_image.paste(image, (x_offset, y_offset))
-        x_offset += offset
-    y_offset += offset
-path = os.path.join(dir_path, '../out/',
-        '{}.png'.format(datetime.datetime.now().strftime("%Y%m%d%H%M%S")))
-new_image.thumbnail((new_image.size[0] / 3, new_image.size[1] / 3), Image.ANTIALIAS)
-new_image.save(path)
+
+def generate_perspectives(f):
+    path = os.path.join(DIR_PATH, '../data/sydney-urban-objects-dataset/objects/', f)
+    xyz = np.loadtxt(open(path, 'rb'), delimiter=',', usecols=range(3,6))
+    x_min, y_min, z_min = xyz.min(axis=0)
+    x_max, y_max, z_max = xyz.max(axis=0)
+    x_center = (x_min + x_max) / 2.0
+    y_center = (y_min + y_max) / 2.0
+    z_center = (z_min + z_max) / 2.0
+    xyz = np.array([(p[0] - x_center, p[1] - y_center, p[2] - z_center) for p in xyz])
+    # v = pptk.viewer(xyz, xyz[:, 0])
+    v = pptk.viewer(xyz)
+     
+    images = []
+    for phi in np.arange(0.0, pi, pi / 8.0):
+        for theta in np.arange(0.0, 2 * pi, pi / 4.0):
+            r = 7.5
+            # camera_x = r * math.sin(phi) * math.cos(theta)
+            # camera_y = r * math.sin(phi) * math.sin(theta)
+            # camera_z = r * math.cos(phi)
+            # colors = []
+            # for p in xyz:
+            #     distance = sqrt((camera_x-p[0])**2 + (camera_y-p[1])**2 + (camera_z-p[2])**2)
+            #     colors.append(distance)
+            # v.attributes(colors)
+            # v.color_map('cool')
+            v.set(phi = phi,
+                  theta = theta,
+                  r = r,
+                  point_size = 0.02,
+                  bg_color = [0, 0, 0, 0],
+                  show_grid = False,
+                  show_info = False,
+                  show_axis = False)
+            time.sleep(SLEEP)
+            path = os.path.join(DIR_PATH, '../out/tmp',
+                '{}-{}.png'.format(phi, theta))
+            images.append(path)
+            v.capture(path)
+    time.sleep(SLEEP)
+    v.close()
+    return images
+
+
+def join_perspectives(images):
+    images = map(Image.open, images) 
+    widths, heights = zip(*(image.size for image in images))
+    total_width = sum(widths)
+    total_height = sum(heights)
+    n = int(sqrt(len(images)))
+    images = [images[i:i+n] for i in xrange(0, len(images), n)]
+    offset = images[0][0].size[0]
+    new_image = Image.new('RGB', (offset * len(images), offset * len(images[0])))
+    y_offset = 0
+    for row in images:
+        x_offset = 0
+        for image in row:
+            new_image.paste(image, (x_offset, y_offset))
+            x_offset += offset
+        y_offset += offset
+    path = os.path.join(DIR_PATH, '../out/',
+            '{}.png'.format(datetime.datetime.now().strftime("%Y%m%d%H%M%S")))
+    new_image.thumbnail((new_image.size[0] / 3, new_image.size[1] / 3), Image.ANTIALIAS)
+    new_image.save(path)
+    return path
+
+
+def make_label(f, object_class):
+    image = Image.open(f) 
+    pre, _ = os.path.splitext(f)
+    f = pre + '.txt'
+    with open(f, 'a') as out:
+        for x in np.arange(0.0, image.width, image.width / 8.0):
+            for y in np.arange(0.0, image.height, image.height / 8.0):
+                out.write(
+                    '{} {} {} {} {}\n'.format(
+                        object_class,
+                        x + image.width / 8.0 / 2.0,
+                        y + image.height / 8.0 / 2.0,
+                        image.width / 8.0,
+                        image.height / 8.0))
+    return f
+
+
+def main():
+    vehicles, pedestrians = enumerate_files()
+    for vehicle in vehicles:
+        perspectives = generate_perspectives(vehicle)
+        f = join_perspectives(perspectives)
+        f = make_label(f, 0)
+        print f
+        return
+
+
+if __name__ == '__main__':
+    main()
