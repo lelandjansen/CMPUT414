3d8776a05af84073289096902f5d85cd647bcc85
Leland Jansen <hello@lelandjansen.com>
2019-04-21 11:57:36 -0600

    More work on report

--- src/visualize.py
+++ src/visualize.py
@@ -11,13 +11,19 @@ import datetime
 import math
 from multiprocessing import Pool
 
+# Sleep between rendering the images and capturing them, due to a bug in pptk
 SLEEP = 0.5
+# Absolute directory containing this script
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 def enumerate_files():
+    '''
+    Enumerate all data files and categorize them as other, vehicle, or pedestrian
+    '''
     path = os.path.join(DIR_PATH, '../data/sydney-urban-objects-dataset/objects/')
     files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     files  = [f for f in files if f.endswith('.csv')]
+    # Types of objects we consider to be "vehicles"
     vehicles = [f for f in files if
             '4wd' in f or
             'bus' in f or
@@ -32,7 +38,11 @@ def enumerate_files():
 
 
 def generate_perspectives(f, style):
+    '''
+    Generate raster perspectives from point cloud data
+    '''
     path = os.path.join(DIR_PATH, '../data/sydney-urban-objects-dataset/objects/', f)
+    # Calculate camera positions
     xyz = np.loadtxt(open(path, 'rb'), delimiter=',', usecols=range(3,6))
     x_min, y_min, z_min = xyz.min(axis=0)
     x_max, y_max, z_max = xyz.max(axis=0)
@@ -40,9 +50,8 @@ def generate_perspectives(f, style):
     y_center = (y_min + y_max) / 2.0
     z_center = (z_min + z_max) / 2.0
     xyz = np.array([(p[0] - x_center, p[1] - y_center, p[2] - z_center) for p in xyz])
-    # v = pptk.viewer(xyz, xyz[:, 0])
     v = pptk.viewer(xyz)
-     
+    # Render the point cloud and rotate the camera. Store the result in `images`
     images = []
     for phi in np.arange(0.0, pi, pi / 8.0):
         for theta in np.arange(0.0, 2 * pi, pi / 4.0):
@@ -50,20 +59,13 @@ def generate_perspectives(f, style):
             if style != 'plain':
                 print 'unknown style'
                 return
+            # Camera position
             x_min, y_min, z_min = xyz.min(axis=0)
             x_max, y_max, z_max = xyz.max(axis=0)
             x_center = (x_min + x_max) / 2.0
             y_center = (y_min + y_max) / 2.0
             z_center = (z_min + z_max) / 2.0
-            # camera_x = r * math.sin(phi) * math.cos(theta)
-            # camera_y = r * math.sin(phi) * math.sin(theta)
-            # camera_z = r * math.cos(phi)
-            # colors = []
-            # for p in xyz:
-            #     distance = sqrt((camera_x-p[0])**2 + (camera_y-p[1])**2 + (camera_z-p[2])**2)
-            #     colors.append(distance)
-            # v.attributes(colors)
-            # v.color_map('cool')
+            # Set camera position
             v.set(lookat = [x_center, y_center, z_center],
                   phi = phi,
                   theta = theta,
@@ -76,6 +78,7 @@ def generate_perspectives(f, style):
             time.sleep(SLEEP)
             path = os.path.join(DIR_PATH, '../out/tmp',
                 '{}-{}.png'.format(phi, theta))
+            # Store the file system path where the temp image will be saved
             images.append(path)
             v.capture(path)
     time.sleep(SLEEP)
@@ -84,6 +87,10 @@ def generate_perspectives(f, style):
 
 
 def join_perspectives(f, images, style):
+    '''
+    Join individual perspective rasters into one raster to be consumed by the
+    classifier.
+    '''
     images = map(Image.open, images) 
     widths, heights = zip(*(image.size for image in images))
     total_width = sum(widths)
@@ -93,6 +100,7 @@ def join_perspectives(f, images, style):
     offset = images[0][0].size[0]
     new_image = Image.new('RGB', (offset * len(images), offset * len(images[0])))
     y_offset = 0
+    # Stitch the images together
     for row in images:
         x_offset = 0
         for image in row:
@@ -102,11 +110,15 @@ def join_perspectives(f, images, style):
     pre, _ = os.path.splitext(f)
     path = os.path.join(DIR_PATH, '../out/', '{}.{}.png'.format(style, pre))
     new_image.thumbnail((new_image.size[0] / 3, new_image.size[1] / 3), Image.ANTIALIAS)
+    # Save the final preprocessed image
     new_image.save(path)
     return path
 
 
 def make_label(f, object_class):
+    '''
+    Generate a structured label for each image (used by YOLO).
+    '''
     image = Image.open(f) 
     pre, _ = os.path.splitext(f)
     f = pre + '.txt'
@@ -127,6 +139,8 @@ def make_label(f, object_class):
 
 
 def main():
+    # For each type, create the perspectives, join them into one image, and
+    # create the necessary labels for YOLO
     types = enumerate_files()
     style = 'plain'
     for t in range(len(types)):
